7134cc96cd0c9aea040fd28addc876a77d6fb437
Muditalbet <muditalbet@gmail.com>
2020-06-22 00:31:45 +0530

    Final Commit To task 2

---
+++ task 2/test/build/precache-manifest.0a89431cddbb0de1875110808850d423.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc52a656e450cad6365cc677bab59650",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1b36328fc583969d5ac",
+    "url": "/static/css/main.ffb4d646.chunk.css"
+  },
+  {
+    "revision": "87b4c884a983fb27b921",
+    "url": "/static/js/2.d97206c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d97206c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b36328fc583969d5ac",
+    "url": "/static/js/main.89186c0f.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  },
+  {
+    "revision": "6d81db685b266efc045c799c8f1cb327",
+    "url": "/static/media/dummy.6d81db68.jpg"
+  }
+]);
